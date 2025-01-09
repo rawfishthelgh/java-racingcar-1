72e64191e5f23fc82e411b142ebf5d822eb46c54
iam-hyo <hyojunj@naver.com>
2025-01-09 22:49:17 +0900

    Scanner 객체 도입

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,8 @@
+package src.main.java;
+
+// Main Class: Calculator.java
+public class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
---
+++ src/test/java/racingCarTest.java
@@ -0,0 +1,2 @@
+public class racingCarTest {
+}
