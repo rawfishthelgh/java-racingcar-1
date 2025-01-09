3e9a841a5aef7ff7bd3a4d9a4724f73eafc00cc1
nicerjs23@gmail.com <nicerjs23@gmail.com>
2025-01-09 21:33:00 +0900

    공동우승 테스트 추가

--- src/test/java/RacingCarGameTest.java
+++ src/test/java/RacingCarGameTest.java
@@ -22,5 +22,22 @@ public class RacingCarGameTest {
         assertEquals(1, cars.get(1).getPosition()); // crong은 1칸
         assertEquals(1, cars.get(2).getPosition()); // honux는 1칸
     }
+    @Test
+    @DisplayName("공동 우승자가 있을 경우 올바르게 출력한다")
+    void shouldFindMultipleWinners() {
+        List<Car> cars = List.of(
+                new Car("pobi"),
+                new Car("crong"),
+                new Car("honux")
+        );
 
+        cars.get(0).move(new FixedMove(4)); // pobi 전진
+        cars.get(1).move(new FixedMove(4)); // crong 전진
+        RacingCarGame game = new RacingCarGame(cars, new FixedMove(0)); // honux 멈춤
+        game.printResult();
+
+        assertEquals(2, cars.get(0).getPosition()); // pobi는 2칸
+        assertEquals(2, cars.get(1).getPosition()); // crong은 2칸
+        assertEquals(1, cars.get(2).getPosition()); // honux는 1칸
+    }
 }
